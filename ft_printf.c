3314534c911ff87a2a41d181beb9b538454c4f02
Mikel Soria Jimenez <msoria-j@c2r3s4.42urduliz.com>
2022-12-12 20:55:07 +0100

    cosas

--- ft_printf.c
+++ ft_printf.c
@@ -6,25 +6,26 @@
 /*   By: msoria-j <msoria-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 16:32:26 by msoria-j          #+#    #+#             */
-/*   Updated: 2022/12/12 19:30:02 by msoria-j         ###   ########.fr       */
+/*   Updated: 2022/12/12 20:34:21 by msoria-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libftprintf.h"
-
+#include<stdio.h>
 int	ft_printf(char const *format, ...)
 {
-	va_list	ap;
-	char	c;
-	char	*s;
-	int		d;
-	long	p;
+	//unsigned int	u;
+	va_list			ap;
+	char			c;
+	char			*s;
+	int				d;
+	long			p;
 	
 	va_start(ap, format);
 	while (*format)
 	{
 		if (*format != '%')
-			write (1, &*format, 1);
+			write (1, format, 1);
 		if (*format == '%')
 		{
 			format++;
@@ -51,6 +52,15 @@ int	ft_printf(char const *format, ...)
 				write(1, "0x", 2);
 				write(1, s, ft_strlen(s));
 			}
+			if (*format == 'u')
+			{
+				d = va_arg(ap, int);
+				s = ft_itoa(d);
+				if (d < 0)
+					write(1, "4294638851", 10);
+				else
+					write(1, s, ft_strlen(s));
+			}			
 		}
 		format++;
 	}
@@ -63,12 +73,17 @@ int	ft_printf(char const *format, ...)
 
 int	main(void)
 {
-	char	c;
-	char	s[] = "test ft_printf";
-	int		d;
+	unsigned int	u;
+	char			c;
+	char			s[] = "test ft_printf";
+	int				d;
+	int				pret;
 
 	c = 'z';
-	d = 12;
-	printf("   printf: %c - %d - %s - %p\n", c, d, s, &c);
-	ft_printf("ft_printf: %c - %d - %s - %p\n", c, d, s, &c);
+	d = -12;
+	u = -650;
+	pret = printf("   printf: %c | %d | %u | %s | %p\n", c, d, u, s, &c);
+	printf("%d\n", pret);
+	pret = ft_printf("ft_printf: %c | %d | %u | %s | %p\n", c, d, u, s, &c);
+	ft_printf("%d\n", pret);
 }
